3e60c484ea84e9968453197ec7e68e5f234cfab8
Silin Na <n1207n@gmail.com>
2016-12-19 16:11:05 -0800

    feat: Added data property and to use them on html template

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="test">
-    <h1>test</h1>
+    <!-- <span v-html="title"></span> -->
+    <h1>{{title}}</h1>
+    <p>{{user.firstName}}</p>
+    <p v-text="user.firstName"></p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'test',
+  data() {
+    return {
+      title: 'Hello world',
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
 };
 </script>
 
