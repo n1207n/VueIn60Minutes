e2d2cd2fa7de481fa83a510deb222eaa14188b2b
Silin Na <n1207n@gmail.com>
2016-12-19 17:05:36 -0800

    feat: Added a router support

--- src/main.js
+++ src/main.js
@@ -2,13 +2,34 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueResource from 'vue-resource';
-import App from './App';
+import VueRouter from 'vue-router';
+// import App from './App';
+import Users from './components/Users';
+import Test from './components/Test';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Users },
+    { path: '/test', component: Test },
+  ],
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App },
-});
+  router,
+  template: `
+    <div id="app">
+      <ul>
+        <li><router-link to="/">Users</router-link></li>
+        <li><router-link to="/test">Test</router-link></li>
+      </ul>
+
+      <router-view></router-view>
+    </div>
+  `,
+}).$mount('#app');
